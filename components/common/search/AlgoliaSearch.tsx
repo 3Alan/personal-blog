6dbd26faea05fbdf9b4e5e75027dde419a16a34b
3Alan <1540703192@qq.com>
2021-08-05 15:02:06 +0800

    fix error key

--- components/common/search/AlgoliaSearch.tsx
+++ components/common/search/AlgoliaSearch.tsx
@@ -1,7 +1,7 @@
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
-const appId = process.env.ALGOLIA_ID;
+const appId = process.env.NEXT_PUBLIC_ALGOLIA_ID;
 const clientKey = process.env.NEXT_PUBLIC_ALGOLIA_CLIENT_KEY;
 
 const searchClient = algoliasearch(appId, clientKey);
