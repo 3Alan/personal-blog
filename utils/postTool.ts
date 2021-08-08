50b2cd6474d494b1cd836b09357d2932d0f7d38e
3Alan <1540703192@qq.com>
2021-08-08 14:06:14 +0800

    debug path

--- utils/postTool.ts
+++ utils/postTool.ts
@@ -4,6 +4,7 @@ import matter from 'gray-matter';
 import { isArray } from './verify';
 
 const postsDirectory = join(process.cwd(), '_posts');
+console.log(postsDirectory, process.cwd(), 'postsDirectory------------');
 
 export function getPostSlugs(): string[] {
   return fs.readdirSync(postsDirectory);
@@ -36,6 +37,8 @@ export function getPostBySlug(slug: string, fields = []): any {
 
 export function getAllPosts(fields = []): any[] {
   const slugs = getPostSlugs();
+  console.log(slugs, 'slugs------');
+
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
