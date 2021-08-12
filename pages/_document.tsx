d8729cfaa00ed8b78de1e8b6e83c756d5fd0b55f
3Alan <1540703192@qq.com>
2021-08-12 13:23:48 +0800

    add baidu analyse

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `var _hmt = _hmt || [];
-                            (function() {
-                            var hm = document.createElement("script");
-                            hm.src = "https://hm.baidu.com/hm.js?e6c2ff650ed2b378b13f2bcdc9aa1e21";
-                            var s = document.getElementsByTagName("script")[0]; 
-                            s.parentNode.insertBefore(hm, s);
-                            })();`
+              (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?291f9c93c57a4ff9e7815c2b5556fa57";
+                var s = document.getElementsByTagName("script")[0]; 
+                s.parentNode.insertBefore(hm, s);
+              })();`
             }}
           ></script>
         </Head>
