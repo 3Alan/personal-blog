efe55d410299c8dfa988e8eedd06f7889f64e8ee
3Alan <1540703192@qq.com>
2021-09-14 18:44:29 +0800

    test: test alan-ui modal component

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,9 +1,10 @@
 import ReactFullpage from '@fullpage/react-fullpage';
-import { FC } from 'react';
-import { Button } from '@3alan/ui';
+import { FC, useState } from 'react';
+import { Button, Modal } from '@3alan/ui';
 import '@3alan/ui/dist/index.css';
 
 const Resume: FC = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <ReactFullpage
       licenseKey="YOUR_KEY_HERE"
@@ -12,9 +13,18 @@ const Resume: FC = () => {
         return (
           <ReactFullpage.Wrapper>
             <div className="section bg-red-400">
-              <Button>123</Button>
+              <Button
+                onClick={() => {
+                  setVisible(true);
+                }}
+              >
+                open
+              </Button>
+              <Modal visible={visible} onClose={() => setVisible(false)}>
+                awesome
+              </Modal>
               <p>Section 1 (welcome to fullpage.js)</p>
-              <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
+              <Button onClick={() => fullpageApi.moveSectionDown()}>move down</Button>
             </div>
             <div className="section bg-yellow-400">
               <p>Section 2</p>
